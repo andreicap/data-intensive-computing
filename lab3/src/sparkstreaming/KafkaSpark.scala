5986c550c8a7ff3f51d056838c1b18f7dc159899
vavassoriluca <vavassoriluca@yahoo.com>
2018-10-06 18:30:26 +0200

    Final push

--- lab3/src/sparkstreaming/KafkaSpark.scala
+++ lab3/src/sparkstreaming/KafkaSpark.scala
@@ -20,18 +20,20 @@ import com.datastax.driver.core.{Session, Cluster, Host, Metadata}
 import com.datastax.spark.connector.streaming._
 
 object KafkaSpark {
+
   def main(args: Array[String]) {
     // connect to Cassandra and make a keyspace and table as explained in the document
     val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
     val session = cluster.connect()
     session.execute("CREATE KEYSPACE IF NOT EXISTS avg_keyspace WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1};")
+    session.execute("DROP TABLE IF EXISTS avg_keyspace.avg;")
     session.execute("CREATE TABLE  IF NOT EXISTS avg_keyspace.avg (word text PRIMARY KEY, count float);")
 
     // make a connection to Kafka and read (key, value) pairs from it
     val conf = new SparkConf().setAppName("avg").setMaster("local[2]")
     val sc = SparkContext.getOrCreate(conf)
     val ssc = new StreamingContext(sc, Seconds(1))    
-
+    ssc.checkpoint("./checkpoint")
 
     val kafkaConf = Map(
         "metadata.broker.list" -> "localhost:9092",
@@ -40,25 +42,38 @@ object KafkaSpark {
         "zookeeper.connection.timeout.ms" -> "1000")
 
     val topics = Set("avg")
-    val messages = KafkaUtils.
-                    createDirectStream( ssc, 
-                                        kafkaConf, 
-                                        topics)
+    
+    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topics)
+
+    val values = messages.map{case (key, value) => value.split(',')}    
+    val pairs = values.map(record => (record(0), record(1).toDouble))
    
     // measure the average value for each key in a stateful manner
     def mappingFunc(key: String, value: Option[Double], state: State[(Double, Int)]): (String, Double) = {
-	   
+	    // get the observations of this letter
+        var oldState: (Double, Int) = state.getOption.getOrElse[(Double, Int)]((0.0, 0))
+        val sum = oldState._1 * oldState._2
+        val newCount = oldState._2 + 1
+        if (value.isEmpty) {
+            state.update(oldState)
+        }
+        else  {
+            val newState: (Double, Int) = ((sum + value.get) / newCount, newCount)
+            state.update(newState)
+        }
+        val output = (key, state.getOption.get._1)
+        output
     }
-    val stateDstream = pairs.mapWithState()//<FILL IN>)
+
+    val stateDstream = pairs.mapWithState(StateSpec.function(mappingFunc _))
 
     // store the result in Cassandra
-    // stateDstream.//<FILL IN>
+    stateDstream.saveToCassandra("avg_keyspace", "avg", SomeColumns("word", "count"))
 
-  //   ssc.start()
-  //   ssc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
-
 
 
 
